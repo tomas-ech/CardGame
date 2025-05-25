d38b698c7db23c88d46627b7116874791f03adf7
tomas96ech <techeverri96@gmail.com>
2025-05-24 21:46:24 -0500

    setup and card prefab

---
+++ Assets/_Scripts/Views/CardView.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class CardView : MonoBehaviour
+{
+    [SerializeField] private TextMeshPro title;
+    [SerializeField] private TextMeshPro description;
+    [SerializeField] private TextMeshPro mana;
+    [SerializeField] private SpriteRenderer image;
+    [SerializeField] private GameObject wrapper;
+}
